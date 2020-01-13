3f7bca0e3a6b24c035b526bf81dfa99fab16021f
martinsgms <martinsgms21@gmail.com>
2020-01-13 00:46:29 -0300

    finaliza crud rest via ajax

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,20 +1,69 @@
 var appCliente = angular.module("appCliente", []);
 
-appCliente.controller("index-controller", function($scope, $http) {
+appCliente.controller("cliente-controller", function($scope, $http) {
     
     $scope.nome = "gabriel";
     $scope.sobrenome = "martins";
-    
-    $http({
-        method:'GET',
-        url:'http://localhost:8080/clientes'
+    $scope.clientes = [];
+    $scope.cliente = {};
+
+
+    $scope.listarClientes = function() {
+        $http({
+            method:'GET',
+            url:'http://localhost:8080/clientes'
+                
+        }).then(function(response) {
+            $scope.clientes = response.data;
             
-    }).then(function(response) {
-        $scope.clientes = response.data;
+        }, function(response) {
+            console.log(response.status);
+            
+        });
+    };
+    
+    $scope.listarClientes();
+    
+    $scope.salvarCliente = function() {
         
-    }, function(response) {
-        console.log(response.status);
+        var verbo = ($scope.cliente.id == null) ? "POST" : "PUT";
         
-    });
+        $http({
+            method: verbo,
+            url:'http://localhost:8080/clientes',
+            data:$scope.cliente
+                
+        }).then(function(response) {
+            $scope.listarClientes();
+            $scope.limpar();
+            
+        }, function(response) {
+            console.log(response.status);
+            console.log(response.data);
+            
+        }); 
+    };
+    
+    $scope.excluirCliente = function(cliente) {
+        $http({
+            method:'DELETE',
+            url:'http://localhost:8080/clientes/' + cliente.id
+                
+        }).then(function(response) {
+            $scope.clientes.splice($scope.clientes.indexOf(cliente), 1);
+            
+        }, function(response) {
+            console.log(response.status);
+            
+        }); 
+    };
+    
+    $scope.preEdit = function(cliente) {
+        $scope.cliente = angular.copy(cliente);
+    };
+    
+    $scope.limpar = function() {
+        $scope.cliente = {};  
+    };
     
 });
